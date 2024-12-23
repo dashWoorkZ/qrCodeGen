bdea5b6c851e48ab3cee7aa408292e6209923bd9
dashWoorkZ <dashwoorkz@dashwoorkz.ca>
2024-12-23 13:37:45 -0800

    Original files provided by Brad Traversy and Traversy Media with edits
    TailWind has been removed from this version of Brad Traversy and Traversy Media's QR Code Generator, with original CSS3 added by Lord :Dash: La Londe of dashWoorkZ Sovereign Society

---
+++ js/script.js
@@ -0,0 +1,87 @@
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
+
+// Button submit
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+
+  clearUI();
+
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
+ 
+
+  // Validate url
+  if (url === "") {
+    alert("Please enter a URL");
+  } else {
+    showSpinner();
+    // Show spinner for 1 sec
+    setTimeout(() => {
+      hideSpinner();
+      generateQRCode(url, size);
+      // Generate the save button after the qr code image src is ready
+      setTimeout(() => {
+        // Get save url
+        const saveUrl = qr.querySelector("canvas").toDataURL();
+        // Create save button
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
+  }
+};
+
+// Generate QR code
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+  
+  // Add width to Main
+  // Add padding to Generated Code
+  const padR = document.querySelector('.qr__code__generator');
+  const mainLg = document.querySelector('#main__container');
+  padR.style.paddingInlineStart = '1rem';
+  mainLg.style.maxWidth = "100%";
+};
+
+// Clear QR code and save button
+const clearUI = () => {
+  qr.innerHTML = "";
+  const saveBtn = document.getElementById("save-link");
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
+
+// Show spinner
+const showSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
+};
+
+// Hide spinner
+const hideSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
+};
+
+// Create save button to download QR code as image
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.style.margin = "0 auto";
+  
+  link.innerHTML = "Save Image";
+
+  link.href = saveUrl;
+  link.download = "qrcode.png";
+
+  document.getElementById("generated").appendChild(link);
+};
+
+hideSpinner();
+
+form.addEventListener("submit", onGenerateSubmit);
